f43375182ac9a61aac37f1d912d19230bd1f442b
fzero <fzero@fzeros-Mac2.local>
2022-03-23 11:49:23 +0700

    add enviroment avarriable 20220323AM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,33 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OkrModule } from './okr/okr.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configuration } from './config/configuration';
+import { validationSchema } from './config/validation';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/okrTestDb',{ useNewUrlParser: true }),OkrModule ],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,      
+      load: [ configuration],
+      validationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST'); 
+        return {
+          uri: `mongodb://${username}:${password}@${host}`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    OkrModule ],
   controllers: [AppController],
   providers: [AppService],
 })
